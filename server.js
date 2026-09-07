50fc93fe289a68916822e0c1a3fbec9b682abab6
Leohaszla <hsnq31565@gmail.com>
2026-09-07 23:28:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const VERIFY_TOKEN = "milano_secret_123";
-const APP_ID = "4221334814824983";
+const INSTAGRAM_APP_ID = "1579440186866459"; 
 const APP_SECRET = "c36718ba55b41c1a0b5c2352bc1b93cf";
 const REDIRECT_URI = "https://fearless-bravery-production.up.railway.app/auth/instagram/callback";
 
